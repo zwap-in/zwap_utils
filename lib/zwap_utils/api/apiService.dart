6b7cde6d10cb0dc66d1587cf29fb44f677fd2e0e
AlessandroMarche <marchettialessandro2001@gmail.com>
2022-03-02 13:13:09 +0100

    Added extra headers to all api call

--- lib/zwap_utils/api/apiService.dart
+++ lib/zwap_utils/api/apiService.dart
@@ -5,18 +5,10 @@ import 'package:http/http.dart' as http;
 import 'dart:developer' as developer;
 
 /// The different types of APIs calls
-enum ApiType{
-  POST,
-  PUT,
-  GET,
-  DELETE,
-  PATCH,
-  HEAD
-}
+enum ApiType { POST, PUT, GET, DELETE, PATCH, HEAD }
 
 /// The base class to call any endpoint
-class ApiService{
-
+class ApiService {
   /// The base URL to call before using any endpoint
   final String baseUrl;
 
@@ -24,11 +16,7 @@ class ApiService{
   final String headerAuthKey;
 
   /// The default custom headers
-  Map<String, String> defaultHeaders = {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'Cache-Control': 'no-cache'
-  };
+  Map<String, String> defaultHeaders = {'Accept': 'application/json', 'Content-Type': 'application/json', 'Cache-Control': 'no-cache'};
 
   ApiService({
     required this.baseUrl,
@@ -36,8 +24,8 @@ class ApiService{
   });
 
   /// It gets the correct apiType name in base of the current ApiType
-  String apiType(ApiType apiType){
-    switch(apiType){
+  String apiType(ApiType apiType) {
+    switch (apiType) {
       case ApiType.POST:
         return "POST";
       case ApiType.PUT:
@@ -54,19 +42,18 @@ class ApiService{
   }
 
   /// Make a get API call
-  Future<http.Response> head(String endpoint, String? token,
-      Map<String, String>? queryData,
-      ){
-    developer.log(
-        'Calling endpoint ' + endpoint,
-        name: "API SERVICE LOG"
-    );
+  Future<http.Response> head(
+    String endpoint,
+    String? token,
+    Map<String, String>? queryData,
+  ) {
+    developer.log('Calling endpoint ' + endpoint, name: "API SERVICE LOG");
     String finalUrl = this.baseUrl + "/" + endpoint;
     Map<String, String> headers = new Map<String, String>.from(this.defaultHeaders);
-    if(token != null){
+    if (token != null) {
       headers[this.headerAuthKey] = "Token $token";
     }
-    if(queryData != null){
+    if (queryData != null) {
       finalUrl += "?";
       queryData.forEach((key, value) {
         finalUrl += "$key=$value&";
@@ -77,113 +64,95 @@ class ApiService{
   }
 
   /// Make a get API call
-  Future<http.Response> get(String endpoint, String? token,
-      Map<String, String>? queryData){
-    developer.log(
-        'Calling endpoint ' + endpoint,
-        name: "API SERVICE LOG"
-    );
+  Future<http.Response> get(String endpoint, String? token, Map<String, String>? queryData, {Map<String, dynamic> extreHeaders = const {}}) {
+    developer.log('Calling endpoint ' + endpoint, name: "API SERVICE LOG");
     Map<String, String> headers = new Map<String, String>.from(this.defaultHeaders);
     String finalUrl = this.baseUrl + "/" + endpoint;
-    if(token != null){
+    if (token != null) {
       headers[this.headerAuthKey] = "Token $token";
     }
-    if(queryData != null){
+    if (queryData != null) {
       finalUrl += "?";
       queryData.forEach((key, value) {
         finalUrl += "$key=$value&";
       });
       finalUrl = finalUrl.substring(0, finalUrl.length - 1);
     }
-    return http.get(Uri.parse(finalUrl), headers: headers);
+    return http.get(Uri.parse(finalUrl), headers: {...headers, ...extreHeaders});
   }
 
   /// Make a put API call
-  Future<http.Response> put(String endpoint, Map<String, dynamic> body,
-      String? token){
-    developer.log(
-        'Calling endpoint ' + endpoint,
-        name: "API SERVICE LOG"
-    );
+  ///
+  /// Extre headers are added after normal headers, so overwrite from extraHeaders is possibile
+  Future<http.Response> put(String endpoint, Map<String, dynamic> body, String? token, {Map<String, dynamic> extreHeaders = const {}}) {
+    developer.log('Calling endpoint ' + endpoint, name: "API SERVICE LOG");
     Map<String, String> headers = new Map<String, String>.from(this.defaultHeaders);
     String finalUrl = this.baseUrl + "/" + endpoint;
-    if(token != null){
+    if (token != null) {
       headers[this.headerAuthKey] = "Token $token";
     }
     String data = jsonEncode(body);
-    return http.put(Uri.parse(finalUrl), headers: headers, body: data);
+    return http.put(Uri.parse(finalUrl), headers: {...headers, ...extreHeaders}, body: data);
   }
 
   /// Make a patch API call
-  Future<http.Response> patch(String endpoint, Map<String, dynamic> body,
-      String? token){
-    developer.log(
-        'Calling endpoint ' + endpoint,
-        name: "API SERVICE LOG"
-    );
+  ///
+  /// Extre headers are added after normal headers, so overwrite from extraHeaders is possibile
+  Future<http.Response> patch(String endpoint, Map<String, dynamic> body, String? token, {Map<String, dynamic> extreHeaders = const {}}) {
+    developer.log('Calling endpoint ' + endpoint, name: "API SERVICE LOG");
     Map<String, String> headers = new Map<String, String>.from(this.defaultHeaders);
     String finalUrl = this.baseUrl + "/" + endpoint;
-    if(token != null){
+    if (token != null) {
       headers[this.headerAuthKey] = "Token $token";
     }
     String data = jsonEncode(body);
-    return http.patch(Uri.parse(finalUrl), headers: headers, body: data);
+    return http.patch(Uri.parse(finalUrl), headers: {...headers, ...extreHeaders}, body: data);
   }
 
   /// Make a delete API call
-  Future<http.Response> delete(String endpoint, String? token,
-      Map<String, dynamic>? deleteData){
-    developer.log(
-        'Calling endpoint ' + endpoint,
-        name: "API SERVICE LOG"
-    );
+  ///
+  /// Extre headers are added after normal headers, so overwrite from extraHeaders is possibile
+  Future<http.Response> delete(String endpoint, String? token, Map<String, dynamic>? deleteData, {Map<String, dynamic> extreHeaders = const {}}) {
+    developer.log('Calling endpoint ' + endpoint, name: "API SERVICE LOG");
     Map<String, String> headers = new Map<String, String>.from(this.defaultHeaders);
     String finalUrl = this.baseUrl + "/" + endpoint;
-    if(token != null){
+    if (token != null) {
       headers[this.headerAuthKey] = "Token $token";
     }
     String data = jsonEncode(deleteData);
-    return http.delete(Uri.parse(finalUrl), headers: headers, body: data);
+    return http.delete(Uri.parse(finalUrl), headers: {...headers, ...extreHeaders}, body: data);
   }
 
   /// Make an API post call
-  Future<http.Response> post(String endpoint, Map<String, dynamic> body,
-      String? token){
-    developer.log(
-        'Calling endpoint ' + endpoint,
-        name: "API SERVICE LOG"
-    );
+  ///
+  /// Extre headers are added after normal headers, so overwrite from extraHeaders is possibile
+  Future<http.Response> post(String endpoint, Map<String, dynamic> body, String? token, {Map<String, dynamic> extreHeaders = const {}}) {
+    developer.log('Calling endpoint ' + endpoint, name: "API SERVICE LOG");
     Map<String, String> headers = new Map<String, String>.from(this.defaultHeaders);
     String finalUrl = this.baseUrl + "/" + endpoint;
-    if(token != null){
+    if (token != null) {
       headers[this.headerAuthKey] = "Token $token";
     }
     String data = jsonEncode(body);
-    return http.post(Uri.parse(finalUrl), headers: headers, body: data);
+    return http.post(Uri.parse(finalUrl), headers: {...headers, ...extreHeaders}, body: data);
   }
 
   /// Make an API post multipart form call
-  Future<http.StreamedResponse> multipartRequest(String endpoint, File fileAsset, String fileFieldName, String? fileName,
-      Map<String, dynamic>? body, String? token, {ApiType apiType = ApiType.POST}) async{
+  Future<http.StreamedResponse> multipartRequest(
+      String endpoint, File fileAsset, String fileFieldName, String? fileName, Map<String, dynamic>? body, String? token,
+      {ApiType apiType = ApiType.POST}) async {
     String currentApiType = this.apiType(apiType);
     var request = http.MultipartRequest(currentApiType, Uri.parse(endpoint));
-    if(token != null){
+    if (token != null) {
       Map<String, String> customHeaders = {this.headerAuthKey: token};
       request.headers.addAll(customHeaders);
     }
-    request.files.add(
-        http.MultipartFile.fromBytes(
-            fileFieldName,
-            fileAsset.readAsBytesSync(),
-            filename: fileName
-        )
-    );
+    request.files.add(http.MultipartFile.fromBytes(fileFieldName, fileAsset.readAsBytesSync(), filename: fileName));
     return request.send();
   }
 
   /// Call this function to simulate an interval seconds to wait before get the fake data from the fake api call
-  Future<void> simulateApiCall({Duration? duration}) async{
+  Future<void> simulateApiCall({Duration? duration}) async {
     await Future.delayed(duration ?? Duration(milliseconds: 80));
   }
-
 }
